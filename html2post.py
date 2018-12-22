7a0c53a5d05ed8004b6e58fb0879455892da5fa0
Eddie Antonio Santos <easantos@ualberta.ca>
2018-12-22 11:46:25 -0700

    Minor improvements; still needs images.

--- html2post.py
+++ html2post.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+# This is free and unencumbered software released into the public domain.
+#
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+#
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# For more information, please refer to <http://unlicense.org/>
+
 """
 Usage:
 
@@ -37,20 +62,21 @@ assert len(author) > 0
 post_content = blog_post.find(class_='entry-content')
 
 # Get rid of ads, sharing buttons, and other junk.
-post_content.find(class_='wpcnt').extract()
-post_content.find(class_='sharedaddy').extract()
+post_content.find(class_='wpcnt').decompose()
+post_content.find(class_='sharedaddy').decompose()
 
-metadata = {
-        'type': 'post',
+front_matter = {
         'title': title,
         'author': author,
 }
 markdown = html2text(str(post_content))
 
-yaml_metadata = '\n'.join(f'{key}: {value!r}' for key, value in metadata.items())
+yaml_front_matter = '\n'.join(f'{key}: {value!r}'
+                              for key, value in front_matter.items())
 
 output = f"""---
-{yaml_metadata}
+layout: post
+{yaml_front_matter}
 ---
 
 {markdown}
